fff30e4bdc808cd80ba79ce65ba0eeee4ad93a08
if18b120 <bastlxfrank@gmail.com>
2024-05-03 15:46:15 +0200

    initial db connection implemented

---
+++ src/electron/database/Connection.ts
@@ -0,0 +1,15 @@
+import { Database, OPEN_READWRITE, OPEN_CREATE } from "sqlite3";
+import fs from "fs";
+
+export class Connection {
+    private db?: Database;
+
+    open() {
+        this.db = new Database('./db/database.sqlite', OPEN_READWRITE);
+    }
+
+    create() {
+        this.db = new Database('./db/database.sqlite', OPEN_CREATE);
+        this.db.exec(fs.readFileSync(__dirname + '/sql/initial-state.sql').toString());
+    }
+}
